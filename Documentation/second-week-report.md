447e0b29f482f80c5c39d53d1b9f04e814a96bc7
Lecromine <laticris@gmail.com>
2016-08-05 01:22:56 +0300

    troubles with the reader class

--- Documentation/second-week-report.md
+++ Documentation/second-week-report.md
@@ -14,3 +14,10 @@ to construct the graph that can be used afterwards as a quicker way to find the
 nearest match for faces. In the graph the faces are already sorted in a way
 so that there's no need to check every face every time. I'm starting to think
 that this problem might be too extensive to be solved in just one month.
+
+This week I had problems with the class Reader.java, and it basically ate up all my time
+I spent with this program. For some reason I can't get it to work and read the .pgm
+files. Internet doesn't offer many solutions for this problem. It's really frustating
+because I can't consentrate on the real problems because of this (debugging is basically
+impossible if I can't read the .pgm files - there's no way to check if the code actually
+works). I hope I'll find the solution soon so I can start working on the core of the program.
--- FaceRecogniser/src/main/java/tilab/facerecogniser/Reader.java
+++ FaceRecogniser/src/main/java/tilab/facerecogniser/Reader.java
@@ -1,11 +1,15 @@
 package tilab.facerecogniser;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /*
@@ -19,26 +23,25 @@ public class Reader {
     }
 
     public void readFiles() throws IOException {
-        
+
         File dir = new File("C:/Users/Lecromine/face-recogniser/FaceRecogniser/facegallery/s1");
 
-        byte[][] faceVec = readFile();
-        
-        
+        int[][] faceVec = readFileB();
+
         System.out.println("width " + faceVec.length);
         System.out.println("height " + faceVec[1].length);
-        
+
         int nonZeros = 0;
-        
+
         for (int i = 0; i < faceVec[1].length; i++) {
-            for (int j = 1000; j < 1050; j++) {
+            for (int j = 0; j < 50; j++) {
                 System.out.print(faceVec[j][i] + ", ");
 
             }
             System.out.println("");
 
         }
-        
+
         System.out.println(nonZeros);
     }
 
@@ -48,65 +51,113 @@ public class Reader {
      * @param path The end of the file path of the face image.
      *
      */
-    public byte[][] readFile() throws FileNotFoundException, IOException {
-
-        //String imagePath = "C:/Users/Lecromine/face-recogniser/FaceRecogniser/facegallery/" + path;
+    public int[][] readFileB() throws FileNotFoundException, IOException {
 
         File dir = new File("C:/Users/Lecromine/face-recogniser/FaceRecogniser/facegallery/s1");
-        
-        int index = 0;
-        
-        byte[][] faceVec = new byte[10340][10];
-        
-        for (File file : dir.listFiles()) {
-            try {
-                FileReader faceImage = new FileReader(file);
-                BufferedReader theReader = new BufferedReader(faceImage);
 
-                String type = theReader.readLine();
-                String heightAndWidth = theReader.readLine();
-                while (heightAndWidth.startsWith("#")) {
-                    heightAndWidth = theReader.readLine();
+        int[][] faceVec = new int[0][0];
+
+        for (File file : dir.listFiles()) {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            Scanner scan = new Scanner(fileInputStream);
+// Discard the magic number
+            scan.nextLine();
+// Discard the comment line
+            scan.nextLine();
+// Read pic width, height and max value
+            int picWidth = scan.nextInt();
+            String picHeight = scan.nextLine();
+            String maxvalue = scan.nextLine();
+
+            fileInputStream.close();
+
+            // Now parse the file as binary data
+            fileInputStream = new FileInputStream(file);
+            DataInputStream dis = new DataInputStream(fileInputStream);
+
+            // look for 4 lines (i.e.: the header) and discard them
+            int numnewlines = 4;
+            while (numnewlines > 0) {
+                char c;
+                do {
+                    c = (char) (dis.readUnsignedByte());
+                } while (c != '\n');
+                numnewlines--;
+            }
+
+            // read the image data
+            faceVec = new int[112][picWidth];
+            for (int row = 0; row < 112; row++) {
+                for (int col = 0; col < picWidth; col++) {
+                    faceVec[row][col] = dis.readUnsignedByte();
+                    System.out.print(faceVec[row][col] + " ");
                 }
-                Scanner s = new Scanner(heightAndWidth);
+                System.out.println();
+            }
+        }
+
+        return faceVec;
+    }
+
+    public byte[][] readFileA() throws FileNotFoundException, IOException {
+
+        //String imagePath = "C:/Users/Lecromine/face-recogniser/FaceRecogniser/facegallery/" + path;
+        File dir = new File("C:/Users/Lecromine/face-recogniser/FaceRecogniser/facegallery/s1");
+
+        int index = 0;
+
+        byte[][] faceVec = new byte[10340][10];
+
+        for (File file : dir.listFiles()) {
+            
+                InputStream f = ClassLoader.getSystemClassLoader().getResourceAsStream("C:/Users/Lecromine/face-recogniser/FaceRecogniser/facegallery/s1/1.pgm");
+                BufferedReader d = new BufferedReader(new InputStreamReader(f));
+                String magic = d.readLine();    // first line contains P2 or P5
+                String line = d.readLine();     // second line contains height and width
+                while (line.startsWith("#")) {
+                    line = d.readLine();
+                }
+                Scanner s = new Scanner(line);
                 int width = s.nextInt();
                 int height = s.nextInt();
-                heightAndWidth = theReader.readLine();
-                s = new Scanner(heightAndWidth);
+                line = d.readLine();// third line contains maxVal
+                s = new Scanner(line);
                 int maxVal = s.nextInt();
-                
+                faceVec = new byte[height][width];
 
                 int count = 0;
                 int b = 0;
                 try {
-                    while (count < 10340) {
-                        b = theReader.read();
+                    while (count < height * width) {
+                        b = d.read();
                         if (b < 0) {
                             break;
                         }
 
-                        if (b == '\n') {
-
-                        } else if ("P5".equals(type)) {
-                            faceVec[count][index] = (byte) ((b >> 8) & 0xFF);
+                        if (b == '\n') { // do nothing if new line encountered
+                        } //                  else if (b == '#') {
+                        //                      d.readLine();
+                        //                  } 
+                        //                  else if (Character.isWhitespace(b)) { // do nothing if whitespace encountered
+                        //                  } 
+                        else if ("P5".equals(magic)) { // Binary format
+                            faceVec[count / width][count % width] = (byte) ((b >> 8) & 0xFF);
                             count++;
-                            faceVec[count][index] = (byte) (b & 0xFF);
+                            faceVec[count / width][count % width] = (byte) (b & 0xFF);
                             count++;
-                        } else {
-                            faceVec[count][index] = (byte) b;
+                        } else {  // ASCII format
+                            faceVec[count / width][count % width] = (byte) b;
                             count++;
                         }
-                        
                     }
-                    index++;
-                    
                 } catch (EOFException eof) {
                     eof.printStackTrace(System.out);
                 }
-
-            } catch (Throwable t) {
-                t.printStackTrace(System.err);
-            }
+                System.out.println("Height=" + height);
+                System.out.println("Width=" + height);
+                System.out.println("Required elements=" + (height * width));
+                System.out.println("Obtained elements=" + count);
+            
 
         }
         return faceVec;
